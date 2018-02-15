ebcf0c1a77a0fc88d31160c5ce1003d4cb954bb5
mgryzio <gryzio92@gmail.com>
2018-02-15 21:13:00 +0100

    Added TestController and test.jsp

--- osk-manager-web/src/main/java/pl/sdacademy/osk/app/web/controller/TestController.java
+++ osk-manager-web/src/main/java/pl/sdacademy/osk/app/web/controller/TestController.java
@@ -3,10 +3,17 @@ package pl.sdacademy.osk.app.web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
+    @RequestMapping(value = {"/", "/main"}, method = RequestMethod.GET)
+    public String test() {
+        LOGGER.debug("is executed");
+        return "test";
+    }
 }
---
+++ osk-manager-web/src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gryzi
+  Date: 15.02.2018
+  Time: 20:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>osk-manager</title>
+</head>
+<body>
+<h1>Witamy w OSK-MANAGER!</h1>
+</body>
+</html>
